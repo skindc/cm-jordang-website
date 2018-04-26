b192f8f815713e81a8c5c4ac228528ebd9243890
skindc <info@skinv1.co.uk>
2018-04-26 15:39:50 +0100

    Add map

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # cm-jordang-website
+
+
+## Plugins
+
+https://getbootstrap.com/docs/4.0/components/carousel/
--- src/js/index.js
+++ src/js/index.js
@@ -9,44 +9,38 @@ $(document).ready(() => {
 
   //If home page
   if (/(.*index\.html|\/)$/.test(location.pathname)) {
+
     //Seems this has to be a class and not id
-    //
-    console.log('load carousel')
     const owl = $(".owl-carousel").owlCarousel({
-      //rtl: true,
-      autoplay: true,
-      autoplayTimeout: 5000,
-      autoplayHoverPause: true,
-      smartSpeed: 1000,
-      //fluidSpeed: true,
-      //Default is linear transitionsMore transitions
-      //can be installed with other plugins
-      //slideTransition: "swing",
-      loop: true,
-      margin: 0,
-      nav: false,
-      //autoWidth: true,
-      //responsiveBaseElement: $(".ncv-home-carousel-content")(),
-      responsive: {
-        0: {
-          items: 1
-        },
-        600: {
-          items: 1
-        },
-        1000: {
-          items: 1
+        //rtl: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        smartSpeed: 1000,
+        //fluidSpeed: true,
+        //Default is linear transitionsMore transitions
+        //can be installed with other plugins
+        //slideTransition: "swing",
+        loop: true,
+        margin: 0,
+        nav: false,
+        //autoWidth: true,
+        //responsiveBaseElement: $(".ncv-home-carousel-content")(),
+        responsive: {
+          0: {
+            items: 1
+          },
+          600: {
+            items: 1
+          },
+          1000: {
+            items: 1
+          }
         }
-      }
-    })
+      }),
 
-
-
-    /*function triggerOwl() {
-
-    }
-
-    setInterval(triggerOwl, 5000)*/
+      map = L.map('ncv-home-location-map').setView([51.505, -0.09], 13)
+      .addLayer(new L.TileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"));
 
   }
 })
